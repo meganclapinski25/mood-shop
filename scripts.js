a6c1c887007cb5bf9f840cf9ff4cbd1229e68ef8
meganclapinski25 <megan.clapinski@students.dominican.edu>
2023-09-23 22:11:36 -0700

    displayed moods with button using javascript

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 import data from './data.js'
 const itemsContainer = document.querySelector('#items')
 
+
 for( let i = 0; i < data.length; i +=1){
     const newDiv = document.createElement('div');
     newDiv.className = 'item';
@@ -14,6 +15,16 @@ for( let i = 0; i < data.length; i +=1){
     console.log(img)
     itemsContainer.appendChild(newDiv);
 
-   
+    const desc = document.createElement('P')
+    desc.innerText = data[i].desc
+    newDiv.appendChild(desc)
+    const price = document.createElement('P')
+    price.innerText = data[i].price
+    newDiv.appendChild(price)
+    const button = document.createElement('button')
+    button.id = data[i].name
+    button.dataset.price = data[i].price
+    button.innerHTML = "Add to Cart"
+    newDiv.appendChild(button)
 
 }
