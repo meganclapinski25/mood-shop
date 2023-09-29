b5b94e102f9a42916eaef83482504c6daae06cda
meganclapinski25 <megan.clapinski@students.dominican.edu>
2023-09-29 16:12:55 -0700

    displayed cart items on console

--- scripts.js
+++ scripts.js
@@ -32,13 +32,54 @@ const cart = []
 
 
 
+console.log(obj)
+
 function addItems(name, price){
-    const item = {name: name, price: price, qty: 1}
+    for(let i = 0; i < cart.length; i+=1){
+        if(cart[i].name === name){
+            cart[i].qty +=1
+            //stop here
+            return
+        }
+    }
+    const item = { name, price, qty: 1}
     cart.push(item);
 
 }
+
+//show items
 function showItems(){
-    console.log(`You have ${cart.length} items in your cart`)
-    console.log(cart.length);
+    const qty = getQty()
+    console.log(`You have ${qty} items in your cart`)
+    for (let i = 0; i < cart.length; i +=1){
+        console.log(`-${cart[i].name} $${cart[i].price} x ${cart[i].qty}`)
+        
+    }
+   
+    console.log(`Total in cart : $${getTotal()}`)
 }
+//Get Quantity
+function getQty(){
+    let qty = 0;
+    for(i =0; i < cart.length; i+=1){
+        qty += cart[i].qty
+    }
+    return qty
+}
+
+
+//Get total
+function getTotal(){
+    let total = 0
+    for(let i = 0; i < cart.length; i +=1){
+        total+=cart[i].price *cart[i].qty
+}
+return total.toFixed(2)
+}
+addItems('apple', .99)
+addItems('orange', .50)
+addItems('pineapple', 1.99)
+addItems('watermelon', 2.99)
+
+showItems()
 
