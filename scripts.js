9258d7af79c341a85a54dab9eb7d3caaedecafbf
meganclapinski25 <megan.clapinski@students.dominican.edu>
2023-10-01 09:31:39 -0700

    cart fully functional

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ const itemsContainer = document.querySelector('#items')
 const itemList = document.getElementById('item-list')
 const cartQty = document.getElementById('cart-qty')
 const cartTotal = document.getElementById('cart-total')
+const addForm = document.getElementById(' add-form')
 
 
 
@@ -47,6 +48,50 @@ all_items_button.forEach(elt => elt.addEventListener('click', () => {
   }))
 //console.log(all_items_button)
 const cart = []
+// handle change events on update input
+itemList.onchange = function(e){
+    if(e.target && e.target.classList.contains('update')){
+        const name = e.target.dataset.name
+        const qty = parseInt(e.target.value)
+
+        updateCart(name, qty)
+
+
+    }
+}
+
+
+
+
+
+
+
+
+// handle clicks on list 
+itemList.onclick = function(e) {
+    //console.log("Clicked List!! ")
+    if(e.target && e.target.classList.contains('remove')){
+       const name = e.target.dataset.name // data-name
+        removeItem(name)
+    }else if(e.target && e.target.classList.contains('add-one')){
+        const name = e.target.dataset.name // data-name
+        addItem(name)
+        //const name = e.target.dataset.name
+        //addItem(name)
+    }else if(e.target && e.target.classList.contains('remove-one')){
+       const name = e.target.dataset.name
+       removeItem(name, 1)
+    }
+}
+
+
+//handle add form submit
+/*addForm.onsubmit = function(e) {
+    e.preventDefault()
+    const name = itemName.value
+    const price = itemPrice.value
+    addItem(name, price)
+}*/
 
 
 
@@ -57,6 +102,7 @@ function addItem(name, price){
     for(let i = 0; i < cart.length; i+=1){
         if(cart[i].name === name){
             cart[i].qty +=1
+            showItems()
             //stop here
             return
         }
@@ -80,7 +126,16 @@ function showItems(){
         
         
        const {name , price, qty} = cart[i]
-        itemStr += `<li> ${name} $${price} x ${qty} =  ${qty * price} </li>`
+
+       
+        itemStr += `<li> 
+        ${name} $${price} x ${qty} =  ${qty * price} 
+        <button class= "remove" data-name = "${name}">Remove</button>
+        <button class= "add-one" data-name = "${name}"> + </button>
+        <button class= "remove-one" data-name = "${name}"> - </button>
+        <input class="update" type ="number" data-name = "${name}">
+
+        </li>`
     }
     itemList.innerHTML = itemStr
 
@@ -104,7 +159,7 @@ function getQty(){
 //Get total
 function getTotal(){
     let total = 0
-    for(let i = 0; i < cart.length; i +=1){
+    for(let i =0; i < cart.length; i+=1){
         total+=cart[i].price *cart[i].qty
 }
 return total.toFixed(2)
@@ -121,10 +176,28 @@ function removeItem(name, qty = 0){
                 cart.splice(i, 1)
             }
             //cart.splice(i, 1)
+            showItems()
             return
         }
     }
 
+}
+
+// updateItem
+function updateCart(name,qty){
+    for(let i = 0; i < cart.length; i+=1){
+        if(cart[i].name === name){
+            if(qty < 1){
+                removeItem(name)
+                return
+            }
+            
+            cart[i].qty = qty
+            showItems()
+            return
+        }
+        
+    }
 }
 
 
